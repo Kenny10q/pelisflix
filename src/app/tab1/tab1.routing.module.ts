9574703928def5297ceb72b841f9d91ba8b9d4b9
kenny10q <kenny26757@gmail.com>
2025-08-12 10:55:31 -0400

    Agregado íconos generados y otros cambios

---
+++ src/app/tab1/tab1.routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'tab1',
+        loadComponent: () => import('../tab1/tab1.module').then(m => m.Tab1Page)
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/tab1',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
